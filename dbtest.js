d6096292ed6a437d53733b7f456907fd4c766351
eea4ue <eea4ue@virginia.edu>
2014-02-18 19:36:45 -0500

    DB Connection updates
    Added my login credentials for DB connection.

--- dbtest.js
+++ dbtest.js
@@ -1,30 +1,26 @@
-// Include http module, 
-var http = require('http'),
-// And mysql module you've just installed. 
-    mysql = require("mysql");
+//Esteban Amas, eea4ue, cs4720 HW4
 
-// Create the connection. 
-// Data is default to new mysql installation and should be changed according to your configuration. 
+// Include http and mysql module 
+var http = require('http'),
+    mysql = require('mysql');
+
+// Create the connection
 var connection = mysql.createConnection({
     host: "stardock.cs.virginia.edu",
-    user: "cs4720mss2x",
-    password: "password",
-    database: "cs4720mss2x"
+    user: "cs4720eea4ue",
+    password: "spring2014",
+    database: "cs4720eea4ue"
 });
 
-// Create the http server. 
+// Create the http server.
 http.createServer(function (request, response) {
     // Query the database. 
-    connection.query('SELECT * FROM sections;', function (error, rows, fields) {
-        response.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        // Send data as JSON string. 
-        // Rows variable holds the result of the query. 
+    connection.query('SELECT * FROM musicData;', function (error, rows, fields) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        // Send data as JSON string. Rows variable holds the result of the query. 
         response.write(JSON.stringify(rows));
         response.end();
     });
-
 // Listen on the 8888 port. 
 }).listen(8888);
 
