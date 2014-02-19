a51ebbdddd7ebc166685558f05ee29eaa913dcce
eea4ue <eea4ue@virginia.edu>
2014-02-19 14:03:35 -0500

    Edits to server file
    commented out port 1337 redirect, cleaned up query and string parsing.

--- server.js
+++ server.js
@@ -4,9 +4,8 @@
 var http = require('http'),
     url = require('url'),
     mysql = require('mysql');
-var port = process.env.PORT || 1337;
+// var port = process.env.PORT || 1337;
 
-// Create the connection
 var connection = mysql.createConnection({
     host: "stardock.cs.virginia.edu",
     user: "cs4720eea4ue",
@@ -14,43 +13,32 @@ var connection = mysql.createConnection({
     database: "cs4720eea4ue"
 });
 
-// Create the http server.
+connection.connect();
+console.log("Connection to DB made.");
+
 http.createServer(function (request, response) {
-    // Query the database. 
-    connection.query('SELECT * FROM musicData;', function (error, rows, fields) {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        // Send data as JSON string. Rows variable holds the result of the query. 
-        response.write(JSON.stringify(rows));
-        response.end();
+    
     var pathname = url.parse(request.url).pathname;
     var url_parts = url.parse(request.url, true);
     var query = url_parts.query;
     console.log("Request for " + pathname + " received.");
-    console.log("Values: " + query["title"]);
+    //console.log("Values: " + query[""]);
 
-    });
-}).listen(port);
+if (query == "") {
+    connection.query("SELECT * FROM musicData WHERE artist = '" + query +"'", function (error, rows, fields) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(JSON.stringify(rows));
+        response.end();
+        });
 
-/*
-function start(response, postData) {
-    console.log("Request handler 'start' was called.");
-
-    var body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html"; '+
-    'charset=UTF-8"  />'+
-    '</head>'+
-    '</body>'+
-    '1. <a href="http://amasnode1.azurewebsites.net/code.zip">A link to a zip file with all of your code</a>'+
-    '2. <a href="http://amasnode1.azurewebsites.net/code.zip">A link to your node.js app with no parameters</a>'+
-    '3. <a href="http://amasnode1.azurewebsites.net/code.zip">A like to your node.js app with at least one parameter</a>'+
-    '</body>'+
-    '</html>';
-
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(body);
-    response.end();
-}
-*/
+} else {
+    connection.query('SELECT * FROM musicData', function (error, rows, fields){
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(JSON.stringify(rows));
+        response.end();
+        });
+    }
+    
+}).listen(8888);
 
 console.log("Server running at http://127.0.0.1:8888/");
