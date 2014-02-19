324d7b5d1740416f3d7c33802033fa8e6eb3f661
eea4ue <eea4ue@virginia.edu>
2014-02-19 16:53:49 -0500

    routing
    routing options

--- server.js
+++ server.js
@@ -18,6 +18,22 @@ console.log("Connection to DB made.");
 
 http.createServer(function (request, response) {
     
+    var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html"; '+
+    'charset=UTF-8"  />'+
+    '</head>'+
+    '</body>'+
+    '1. <a href="http://amasnode1.azurewebsites.net/code.zip">A link to a zip file with all of your code</a><br>'+
+    '2. <a href="localhost:8888">A link to your node.js app with no parameters</a><br>'+
+    '3. <a href="localhost:8888/Spoon">A link to your node.js app with at least one parameter</a><br>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    //response.end();
+
     var pathname = url.parse(request.url).pathname;
     var url_parts = url.parse(request.url, true); //parse query string
     var query = url_parts.query;
@@ -28,22 +44,22 @@ http.createServer(function (request, response) {
     console.log( JSON.stringify(query));
     console.log(artistName);
 
-if (pathname == "" || pathname == "/") {
-    connection.query("SELECT * FROM musicData", function (error, rows, fields) {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(JSON.stringify(rows));
-        response.end();
-        });
+    if (pathname == "" || pathname == "/") {
+        connection.query("SELECT * FROM musicData", function (error, rows, fields) {
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.write(JSON.stringify(rows));
+            response.end();
+            });
 
-} else {
-        connection.query("SELECT * FROM musicData WHERE artist = '" + artistName + "'", function (error, rows, fields) {
-        //console.log("Values: " + query[]);
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(JSON.stringify(rows));
-        response.end();
-        });
+    } else {
+            connection.query("SELECT * FROM musicData WHERE artist = '" + artistName + "'", function (error, rows, fields) {
+            //console.log("Values: " + query[]);
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.write(JSON.stringify(rows));
+            response.end();
+            });
 
-    }
+        }
     
 }).listen(8888);
 
